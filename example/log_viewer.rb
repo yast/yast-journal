bd81fd234bc1d501c0eea4dea68c972355fe6e0f
Josef Reidinger <jreidinger@suse.cz>
2018-06-12 13:35:37 +0200

    and also extect example for easier testing

--- example/log_viewer.rb
+++ example/log_viewer.rb
@@ -3,8 +3,8 @@ require "yast"
 require "systemd_journal/journal_viewer"
 require "systemd_journal/entry"
 
-param = Yast::WFM.Args[0]
-raise "missing param for log entries" unless param
+params = Yast::WFM.Args
+raise "missing param for log entries" if params.empty?
 
-entries = SystemdJournal::Entry.all(options: {"unit" => param, "boot" => nil})
+entries = SystemdJournal::Entry.all(options: {"unit" => params, "boot" => nil})
 SystemdJournal::JournalViewer.new(entries).run
